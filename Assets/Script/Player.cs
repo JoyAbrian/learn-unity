8f7c90a6b78bff6e1a13dcea797c25c2a2d611a9
JoyAbrian <joyabrianrantepasang14@gmail.com>
2023-12-28 16:44:18 +0800

    feat: nambah efek tabrakan

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -12,7 +12,44 @@ public class Player : MonoBehaviour
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (!canMove)
+        {
+            // Can't move towards
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            // Attempt only X movements
+            if (canMove)
+            {
+                // Can move only X
+                moveDir = moveDirX;
+            } else
+            {
+                // Can't move only on X
+
+                // Attempt Z movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    // Can move only Z
+                    moveDir = moveDirZ;
+                }
+            }
+        }
+
+        if (canMove)
+        {
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+        }
+
         isWalking = moveDir != Vector3.zero;
 
         float rotateSpeed = 10f;
