66cc892b4bbe9eb02c8547241f9bb885777d33c6
JoyAbrian <joyabrianrantepasang14@gmail.com>
2023-12-28 10:49:15 +0800

    feat: nambah animasi karakter

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    private bool isWalking;
 
     private void Update()
     {
@@ -24,8 +25,14 @@ public class Player : MonoBehaviour
         inputVector = inputVector.normalized;
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+        isWalking = moveDir != Vector3.zero;
 
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
