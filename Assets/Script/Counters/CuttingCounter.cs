b6797a4ee8478a8050dafd9619086818dcf8216e
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-03-08 14:00:27 +0800

    Revert "feat: bisa tambah bahan ke piring"
    This reverts commit 612f9b492485fe430f16014ed296a705d6fc1410.

--- Assets/Script/Counters/CuttingCounter.cs
+++ Assets/Script/Counters/CuttingCounter.cs
@@ -47,15 +47,8 @@ public class CuttingCounter : BaseCounter, IHasProgress
             if (player.HasKitchenObject())
             {
                 // Player is carrying something
-                if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
-                {
-                    // Player is holding a plate
-                    if (plateKitchenObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO()))
-                    {
-                        GetKitchenObject().DestroySelf();
-                    }
-                }
-            } else
+            }
+            else
             {
                 // Player is not carrying anything
                 GetKitchenObject().setKitchenObjectParent(player);
